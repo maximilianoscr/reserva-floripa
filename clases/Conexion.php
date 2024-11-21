8be491556a4c102e9341d7e4b178825e561d1f13
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2024-11-21 20:40:51 -0300

    Pruebas

--- clases/Conexion.php
+++ clases/Conexion.php
@@ -1,9 +1,9 @@
 <?php
     class Conexion {
-        public $servidor = 'localhost';
-        public $usuario = 'root';
-        public $password = '';
-        public $database = 'floripa';
+        public $servidor = 'sql103.infinityfree.com';
+        public $usuario = 'if0_37759234';
+        public $password = 'thr8TxXIiqky2zt';
+        public $database = 'if0_37759234_floripa';
         //public $database = 'floripa';
         public $port = 3306;
 
