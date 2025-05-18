cf2f741342aec93db1811f0c0c17f100f3ffb90e
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2025-05-18 02:22:29 -0300

    Actualizo url

--- registro/index.php
+++ registro/index.php
@@ -7,8 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/registro.css">
-    <link rel="stylesheet" href="public/css/estilos.css">
+    <link rel="stylesheet" href="../public/css/registro.css">
+    <link rel="stylesheet" href="../public/css/estilos.css">
     <title>Registro de usuario</title>
   </head>
   <body>
