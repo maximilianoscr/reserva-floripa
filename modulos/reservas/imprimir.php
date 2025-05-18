b859f6aae36bd89b25f320c0edc7bed851f5e008
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2025-05-18 03:57:16 -0300

    Algunos avances dle boceto impresion

---
+++ modulos/reservas/imprimir.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../public/css/confirmacion.css">
+  <title>Confirmaci&oacute;n de Reserva - Reserva·Web</title>
+</head>
+<body>
+
+  <header>
+    <img src="../../public/img/logo2.png" alt="Reserva·Web" width="80" />
+    <h1>CONFIRMACI&Oacute;N DE RESERVA - HOLA NINAAAAAAAA</h1>
+    <h2>Mart&iacute;n Rodr&iacute;guez</h2>
+  </header>
+
+  <section class="container">
+    <div class="info">
+      <div>
+        <strong>Fecha de entrada:</strong><br />
+        10 de marzo de 2025
+      </div>
+      <div>
+        <strong>Fecha de salida:</strong><br />
+        15 de marzo de 2025
+      </div>
+      <div>
+        <strong>Departamento en Canasvieiras:</strong><br />
+        2 hu&eacute;spedes
+      </div>
+      <div>
+        <strong>Desayuno incluido</strong>
+      </div>
+    </div>
+
+    <p><strong>C&oacute;digo de reserva:</strong> 5829KH</p>
+
+    <button class="btn-contactar">CONTACTAR</button>
+  </section>
+
+</body>
+</html>
