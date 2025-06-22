cfae70f4898e1e6f1e1ccb5acf6b15bc35525606
maximilianoscr <135906987+maximilianoscr@users.noreply.github.com>
2025-06-22 20:05:58 -0300

    Correccion

--- modulos/propietario/tabla_propietario.php
+++ modulos/propietario/tabla_propietario.php
@@ -1,5 +1,5 @@
 <?php session_start(); 
-    include "../../clases/Propietario.php";
+    include "../../clases/Propietarios.php";
     $Propietario = new Propietarios();
     $items = $Propietario->mostrarPropietarios($_SESSION['id_usuario']);
 ?>
